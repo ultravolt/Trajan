6be0f35fa46842a1c603c465f4f993919dd79b4c
Alex Padilla <alex.padilla@uphs.upenn.edu>
2016-12-07 13:55:07 -0500

    Added Trajan Tile definitions

--- Trajan/TrajanStack.cs
+++ Trajan/TrajanStack.cs
@@ -12,16 +12,98 @@ namespace Trajan
         {
         }
     }
-    public class TrajanStacks : Dictionary<TrajanTile.TYPE,TrajanStack>
+    public class TrajanStacks : Dictionary<TrajanTile.TYPE, TrajanStack>
     {
         public TrajanStacks()
         {
-            var types = Enum.GetValues(typeof(TrajanTile.TYPE)).Cast<TrajanTile.TYPE>().Where(x=>x!=TrajanTile.TYPE.UNDEFINED);            
+            var types = Enum.GetValues(typeof(TrajanTile.TYPE)).Cast<TrajanTile.TYPE>();
+            var pi = new Dictionary<TrajanTile.TYPE, List<TrajanTile>>();
+            pi.Add(TrajanTile.TYPE.VICTORY, new List<TrajanTile>
+                {
+                    new TrajanTile(TrajanTile.TYPE.VICTORY,ActionMarker.COLOR.BLUE, ActionMarker.COLOR.BLUE, TrajanTile.POINT_VALUE.NINE),
+                    new TrajanTile(TrajanTile.TYPE.VICTORY,ActionMarker.COLOR.ORANGE, ActionMarker.COLOR.ORANGE, TrajanTile.POINT_VALUE.NINE),
+                    new TrajanTile(TrajanTile.TYPE.VICTORY,ActionMarker.COLOR.PINK, ActionMarker.COLOR.PINK, TrajanTile.POINT_VALUE.NINE),
+                    new TrajanTile(TrajanTile.TYPE.VICTORY,ActionMarker.COLOR.GREEN, ActionMarker.COLOR.GREEN, TrajanTile.POINT_VALUE.NINE),
+                    new TrajanTile(TrajanTile.TYPE.VICTORY,ActionMarker.COLOR.YELLOW, ActionMarker.COLOR.YELLOW, TrajanTile.POINT_VALUE.NINE),
+                    new TrajanTile(TrajanTile.TYPE.VICTORY,ActionMarker.COLOR.WHITE, ActionMarker.COLOR.WHITE, TrajanTile.POINT_VALUE.NINE),
+                    new TrajanTile(TrajanTile.TYPE.VICTORY,ActionMarker.COLOR.BLUE, ActionMarker.COLOR.WHITE, TrajanTile.POINT_VALUE.NINE),
+                    new TrajanTile(TrajanTile.TYPE.VICTORY,ActionMarker.COLOR.YELLOW, ActionMarker.COLOR.WHITE, TrajanTile.POINT_VALUE.NINE),
+                    new TrajanTile(TrajanTile.TYPE.VICTORY,ActionMarker.COLOR.ORANGE, ActionMarker.COLOR.WHITE, TrajanTile.POINT_VALUE.NINE)
+                });
+
+            pi.Add(TrajanTile.TYPE.ACTION_EFFICIENCY, new List<TrajanTile>
+                {
+                    new TrajanTile(TrajanTile.TYPE.ACTION_EFFICIENCY,ActionMarker.COLOR.PINK, ActionMarker.COLOR.BLUE),
+                    new TrajanTile(TrajanTile.TYPE.ACTION_EFFICIENCY,ActionMarker.COLOR.PINK, ActionMarker.COLOR.ORANGE),
+                    new TrajanTile(TrajanTile.TYPE.ACTION_EFFICIENCY,ActionMarker.COLOR.PINK, ActionMarker.COLOR.GREEN),
+                    new TrajanTile(TrajanTile.TYPE.ACTION_EFFICIENCY,ActionMarker.COLOR.PINK, ActionMarker.COLOR.YELLOW),
+                    new TrajanTile(TrajanTile.TYPE.ACTION_EFFICIENCY,ActionMarker.COLOR.PINK, ActionMarker.COLOR.WHITE),
+                    new TrajanTile(TrajanTile.TYPE.ACTION_EFFICIENCY,ActionMarker.COLOR.PINK, ActionMarker.COLOR.WHITE),
+                    new TrajanTile(TrajanTile.TYPE.ACTION_EFFICIENCY,ActionMarker.COLOR.PINK, ActionMarker.COLOR.PINK),
+                    new TrajanTile(TrajanTile.TYPE.ACTION_EFFICIENCY,ActionMarker.COLOR.WHITE, ActionMarker.COLOR.WHITE),
+                    new TrajanTile(TrajanTile.TYPE.ACTION_EFFICIENCY,ActionMarker.COLOR.GREEN, ActionMarker.COLOR.WHITE)
+                });
+
+            pi.Add(TrajanTile.TYPE.LABOR, new List<TrajanTile>
+                {
+                    new TrajanTile(TrajanTile.TYPE.LABOR,ActionMarker.COLOR.ORANGE, ActionMarker.COLOR.ORANGE, TrajanTile.POINT_VALUE.TWO, TrajanTile.WORKER_BONUS.TWO),
+                    new TrajanTile(TrajanTile.TYPE.LABOR,ActionMarker.COLOR.YELLOW, ActionMarker.COLOR.YELLOW, TrajanTile.POINT_VALUE.TWO, TrajanTile.WORKER_BONUS.TWO),
+                    new TrajanTile(TrajanTile.TYPE.LABOR,ActionMarker.COLOR.YELLOW, ActionMarker.COLOR.ORANGE, TrajanTile.POINT_VALUE.TWO, TrajanTile.WORKER_BONUS.TWO),
+                    new TrajanTile(TrajanTile.TYPE.LABOR,ActionMarker.COLOR.YELLOW, ActionMarker.COLOR.BLUE, TrajanTile.POINT_VALUE.TWO, TrajanTile.WORKER_BONUS.TWO),
+
+                    new TrajanTile(TrajanTile.TYPE.LABOR,ActionMarker.COLOR.ORANGE, ActionMarker.COLOR.GREEN, TrajanTile.POINT_VALUE.FIVE, TrajanTile.WORKER_BONUS.ONE),
+                    new TrajanTile(TrajanTile.TYPE.LABOR,ActionMarker.COLOR.ORANGE, ActionMarker.COLOR.PINK, TrajanTile.POINT_VALUE.FIVE, TrajanTile.WORKER_BONUS.ONE),
+                    new TrajanTile(TrajanTile.TYPE.LABOR,ActionMarker.COLOR.ORANGE, ActionMarker.COLOR.YELLOW, TrajanTile.POINT_VALUE.FIVE, TrajanTile.WORKER_BONUS.ONE),
+                    new TrajanTile(TrajanTile.TYPE.LABOR,ActionMarker.COLOR.ORANGE, ActionMarker.COLOR.WHITE, TrajanTile.POINT_VALUE.FIVE, TrajanTile.WORKER_BONUS.ONE),
+                    new TrajanTile(TrajanTile.TYPE.LABOR,ActionMarker.COLOR.BLUE, ActionMarker.COLOR.GREEN, TrajanTile.POINT_VALUE.FIVE, TrajanTile.WORKER_BONUS.ONE),
+
+                });
+
+            pi.Add(TrajanTile.TYPE.DEMAND, new List<TrajanTile>
+                {
+                    new TrajanTile(TrajanTile.TYPE.DEMAND,ActionMarker.COLOR.GREEN, ActionMarker.COLOR.GREEN, DemandTile.TYPE.BREAD),
+                    new TrajanTile(TrajanTile.TYPE.DEMAND,ActionMarker.COLOR.GREEN, ActionMarker.COLOR.ORANGE, DemandTile.TYPE.BREAD),
+                    new TrajanTile(TrajanTile.TYPE.DEMAND,ActionMarker.COLOR.PINK, ActionMarker.COLOR.PINK, DemandTile.TYPE.BREAD),
+
+                    new TrajanTile(TrajanTile.TYPE.DEMAND,ActionMarker.COLOR.GREEN, ActionMarker.COLOR.GREEN, DemandTile.TYPE.GAMES),
+                    new TrajanTile(TrajanTile.TYPE.DEMAND,ActionMarker.COLOR.GREEN, ActionMarker.COLOR.YELLOW, DemandTile.TYPE.GAMES),
+                    new TrajanTile(TrajanTile.TYPE.DEMAND,ActionMarker.COLOR.GREEN, ActionMarker.COLOR.PINK, DemandTile.TYPE.GAMES),
+
+                    new TrajanTile(TrajanTile.TYPE.DEMAND,ActionMarker.COLOR.WHITE, ActionMarker.COLOR.WHITE, DemandTile.TYPE.RELIGION),
+                    new TrajanTile(TrajanTile.TYPE.DEMAND,ActionMarker.COLOR.GREEN, ActionMarker.COLOR.WHITE, DemandTile.TYPE.RELIGION),
+                    new TrajanTile(TrajanTile.TYPE.DEMAND,ActionMarker.COLOR.BLUE, ActionMarker.COLOR.ORANGE, DemandTile.TYPE.RELIGION),
+                });
+
+            pi.Add(TrajanTile.TYPE.CARDS, new List<TrajanTile>
+            {
+                    new TrajanTile(TrajanTile.TYPE.CARDS, ActionMarker.COLOR.BLUE, ActionMarker.COLOR.BLUE),
+                    new TrajanTile(TrajanTile.TYPE.CARDS, ActionMarker.COLOR.YELLOW, ActionMarker.COLOR.YELLOW),
+                    new TrajanTile(TrajanTile.TYPE.CARDS, ActionMarker.COLOR.ORANGE, ActionMarker.COLOR.ORANGE),
+                    new TrajanTile(TrajanTile.TYPE.CARDS, ActionMarker.COLOR.BLUE, ActionMarker.COLOR.WHITE),
+                    new TrajanTile(TrajanTile.TYPE.CARDS, ActionMarker.COLOR.BLUE, ActionMarker.COLOR.ORANGE),
+                    new TrajanTile(TrajanTile.TYPE.CARDS, ActionMarker.COLOR.ORANGE, ActionMarker.COLOR.YELLOW),
+                    new TrajanTile(TrajanTile.TYPE.CARDS, ActionMarker.COLOR.YELLOW, ActionMarker.COLOR.GREEN),
+                    new TrajanTile(TrajanTile.TYPE.CARDS, ActionMarker.COLOR.GREEN, ActionMarker.COLOR.PINK),
+                    new TrajanTile(TrajanTile.TYPE.CARDS, ActionMarker.COLOR.WHITE, ActionMarker.COLOR.PINK),
+                });
+
+            pi.Add(TrajanTile.TYPE.MILITARY, new List<TrajanTile>
+            {
+                    new TrajanTile(TrajanTile.TYPE.MILITARY,ActionMarker.COLOR.ORANGE, ActionMarker.COLOR.ORANGE, TrajanTile.POINT_VALUE.TWO, TrajanTile.LEGIONAIRE_BONUS.TWO),
+                    new TrajanTile(TrajanTile.TYPE.MILITARY,ActionMarker.COLOR.YELLOW, ActionMarker.COLOR.YELLOW, TrajanTile.POINT_VALUE.TWO, TrajanTile.LEGIONAIRE_BONUS.TWO),
+                    new TrajanTile(TrajanTile.TYPE.MILITARY,ActionMarker.COLOR.YELLOW, ActionMarker.COLOR.ORANGE, TrajanTile.POINT_VALUE.TWO, TrajanTile.LEGIONAIRE_BONUS.TWO),
+                    new TrajanTile(TrajanTile.TYPE.MILITARY,ActionMarker.COLOR.YELLOW, ActionMarker.COLOR.BLUE, TrajanTile.POINT_VALUE.TWO, TrajanTile.LEGIONAIRE_BONUS.TWO),
+
+                    new TrajanTile(TrajanTile.TYPE.MILITARY,ActionMarker.COLOR.ORANGE, ActionMarker.COLOR.GREEN, TrajanTile.POINT_VALUE.FIVE, TrajanTile.LEGIONAIRE_BONUS.ONE),
+                    new TrajanTile(TrajanTile.TYPE.MILITARY,ActionMarker.COLOR.ORANGE, ActionMarker.COLOR.PINK, TrajanTile.POINT_VALUE.FIVE, TrajanTile.LEGIONAIRE_BONUS.ONE),
+                    new TrajanTile(TrajanTile.TYPE.MILITARY,ActionMarker.COLOR.ORANGE, ActionMarker.COLOR.YELLOW, TrajanTile.POINT_VALUE.FIVE, TrajanTile.LEGIONAIRE_BONUS.ONE),
+                    new TrajanTile(TrajanTile.TYPE.MILITARY,ActionMarker.COLOR.ORANGE, ActionMarker.COLOR.WHITE, TrajanTile.POINT_VALUE.FIVE, TrajanTile.LEGIONAIRE_BONUS.ONE),
+                    new TrajanTile(TrajanTile.TYPE.MILITARY,ActionMarker.COLOR.BLUE, ActionMarker.COLOR.GREEN, TrajanTile.POINT_VALUE.FIVE, TrajanTile.LEGIONAIRE_BONUS.ONE),
+
+                     });
             foreach (var type in types)
             {
-                var dl = new List<TrajanTile>();
-                for (int j = 0; j < 9; j++)
-                    dl.Add(new TrajanTile(type));
+                var dl = pi[type];
                 ListExtensions.Shuffle(dl);
                 this[type] = new TrajanStack(dl);
             }
--- Trajan/TrajanTile.cs
+++ Trajan/TrajanTile.cs
@@ -6,11 +6,33 @@ using System.Threading.Tasks;
 
 namespace Trajan
 {
-    public class TrajanTile
+    public class TrajanTile : Tuple<ActionMarker.COLOR, ActionMarker.COLOR>
     {
+
+        public enum WORKER_BONUS
+        {
+            ONE = 1,
+            TWO = 2
+        }
+
+        public enum LEGIONAIRE_BONUS
+        {
+            ONE = 1,
+            TWO = 2
+        }
+
+        public enum POINT_VALUE
+        {
+            ONE = 1,
+            TWO = 2,
+            THREE = 3,
+            FOUR = 4,
+            FIVE = 5,
+            NINE = 9
+        }
+
         public enum TYPE
         {
-            UNDEFINED,
             LABOR,
             MILITARY,
             VICTORY,
@@ -18,13 +40,42 @@ namespace Trajan
             DEMAND,
             ACTION_EFFICIENCY
         }
+        public POINT_VALUE Points { get; set; }
+        public LEGIONAIRE_BONUS LegionaireBonus { get; private set; }
+        public WORKER_BONUS WorkerBonus { get; private set; }
+        public DemandTile.TYPE DemandType { get; private set; }
+        
 
         public TrajanTile.TYPE TrajanType { get; private set; }
 
 
-        public TrajanTile(TrajanTile.TYPE type = TrajanTile.TYPE.UNDEFINED)
+        public TrajanTile(TrajanTile.TYPE type, ActionMarker.COLOR i1, ActionMarker.COLOR i2) : base(i1, i2)
         {
             this.TrajanType = type;
+        }
+        public TrajanTile(TrajanTile.TYPE type, ActionMarker.COLOR i1, ActionMarker.COLOR i2, DemandTile.TYPE demandType) : base(i1, i2)
+        {
+            this.TrajanType = type;
+            this.DemandType = demandType;
+        }
+        public TrajanTile(TrajanTile.TYPE type, ActionMarker.COLOR item1, ActionMarker.COLOR item2, POINT_VALUE pointValue) : base(item1, item2)
+        {
+            this.TrajanType = type;
+            this.Points = pointValue;
+        }
+
+        public TrajanTile(TrajanTile.TYPE type, ActionMarker.COLOR item1, ActionMarker.COLOR item2, POINT_VALUE pointValue, WORKER_BONUS workerBonus) : base(item1, item2)
+        {
+            this.TrajanType = type;
+            this.Points = pointValue;
+            this.WorkerBonus = workerBonus;
+        }
+
+        public TrajanTile(TrajanTile.TYPE type, ActionMarker.COLOR item1, ActionMarker.COLOR item2, POINT_VALUE pointValue, LEGIONAIRE_BONUS legionaireBonus) : base(item1, item2)
+        {
+            this.TrajanType = type;
+            this.Points = pointValue;
+            this.LegionaireBonus = legionaireBonus;
         }
 
         public override string ToString()
